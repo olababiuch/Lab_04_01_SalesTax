e04531c4608bf8253691fa3979821d0088d4ef3f
olababiuch <ola.babiuch7@gmail.com>
2025-06-15 02:12:48 -0400

    Task 1 completed

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    int itemPrice = 10;
+    double TAX_RATE = 0.05;
+
+    double salesTax = itemPrice * TAX_RATE;
+
+    System.out.println("The tax on a purchase of $" + itemPrice + " is $" + salesTax);
+
+
     }
 }
